669dd501fea1fc65fb8b5fc604cf3edc4f661124
Alireza JavadZadeh <alirezajavadzade76@gmail.com>
2025-04-23 10:59:26 +0330

    build: v 1.16.0

---
+++ dist/assets/price-BnWQezQL.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./index-gvta-7jh.js";const s={async GoldPrice(){return(await e.get("/goldPrice")).data}};export{s as P};
--- dist/assets/price-IwKSuLX2.js
+++
@@ -1,1 +0,0 @@
-import{Q as e}from"./index-D6UDiPus.js";const s={async GoldPrice(){return(await e.get("/goldPrice")).data}};export{s as P};
